d96aa9db1577459dac4992e40f6ccb31a1b09583
Martynas Druteika <martynas.druteika@devbridge.com>
2023-01-03 19:14:15 +0200

    Nodejs setup

---
+++ app/app.js
@@ -0,0 +1,21 @@
+const cors = require('cors');
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const mysqlConfig = {
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'test123',
+    database: 'expenses_tracker',
+    port: 3306
+};
+
+const connection = mysql.createConnection(mysqlConfig);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
