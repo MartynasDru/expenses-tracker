facd1e53e8c48ed199c1d286abff0db148903856
Martynas Druteika <martynas.druteika@devbridge.com>
2023-01-03 20:01:08 +0200

    expenses endpoint

--- app/app.js
+++ app/app.js
@@ -17,5 +17,11 @@ const mysqlConfig = {
 
 const connection = mysql.createConnection(mysqlConfig);
 
+app.get('/expenses', (req, res) => {
+    connection.execute('SELECT * FROM expenses', (err, expenses) => {
+        res.send(expenses);
+    });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
