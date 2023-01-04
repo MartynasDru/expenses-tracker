6136b4d4fde11a03984e87041c9ae463fcf4e08b
Martynas Druteika <martynas.druteika@devbridge.com>
2023-01-04 20:07:15 +0200

    Expenses list styling

--- client/src/pages/Expenses/Expenses.jsx
+++ client/src/pages/Expenses/Expenses.jsx
@@ -1,6 +1,40 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import styled from 'styled-components';
 import { API_URL, LOGGED_IN_USER } from "../../constants/constants";
+
+const ExpensesList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    list-style: none;
+`;
+
+const ExpensesListItem = styled.li`
+    border-radius: 10px;
+    box-shadow: 0 5px 7px -1px rgb(51 51 51 / 23%);
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 30px;
+`;
+
+const ExpenseAmount = styled.span`
+    align-items: center;
+    color: #35d8ac;
+    display: flex;
+    font-size: 34px;
+    font-weight: 700;
+`;
+
+const ExpenseType = styled.span`
+    align-items: center;
+    color: #979cb0;
+    display: flex;
+    font-size: 20px;
+    font-weight: 600;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: auto;
+`;
 
 export const Expenses = () => {
     const [expenses, setExpenses] = useState([]);
@@ -20,8 +54,13 @@ export const Expenses = () => {
     }
 
     return (
-        <div>
-            {expenses.map((exp) => <div key={exp.id}>{exp.amount}</div>)}
-        </div>
+        <ExpensesList>
+            {expenses.map((exp) => (
+                <ExpensesListItem key={exp.id}>
+                    <ExpenseType>{exp.type}</ExpenseType>
+                    <ExpenseAmount>€{exp.amount}</ExpenseAmount>
+                </ExpensesListItem>
+            ))}
+        </ExpensesList>
     );
 }
