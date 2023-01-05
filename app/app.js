ac6bebd021c74be4621724dc606e1d51120f3e08
Martynas Druteika <martynas.druteika@devbridge.com>
2023-01-05 19:01:10 +0200

    Introduce env variables for Nodejs server

--- app/app.js
+++ app/app.js
@@ -2,17 +2,19 @@ const cors = require('cors');
 const express = require('express');
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 const mysqlConfig = {
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'test123',
-    database: 'expenses_tracker',
-    port: 3306
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT
 };
 
 const connection = mysql.createConnection(mysqlConfig);
