c2da29d21584d084483deb6953dc1dba8c840087
Martynas Druteika <martynas.druteika@devbridge.com>
2023-01-05 18:21:59 +0200

    Truncate expense type text when does not fit

--- client/src/pages/Expenses/Expenses.jsx
+++ client/src/pages/Expenses/Expenses.jsx
@@ -10,6 +10,7 @@ const ExpensesList = styled.ul`
 `;
 
 const ExpensesListItem = styled.li`
+    align-items: center;
     border-radius: 10px;
     box-shadow: 0 5px 7px -1px rgb(51 51 51 / 23%);
     display: flex;
@@ -18,22 +19,18 @@ const ExpensesListItem = styled.li`
 `;
 
 const ExpenseAmount = styled.span`
-    align-items: center;
     color: #35d8ac;
-    display: flex;
     font-size: 34px;
     font-weight: 700;
 `;
 
 const ExpenseType = styled.span`
-    align-items: center;
     color: #979cb0;
-    display: flex;
     font-size: 20px;
     font-weight: 600;
     text-overflow: ellipsis;
     white-space: nowrap;
-    overflow: auto;
+    overflow: hidden;
 `;
 
 export const Expenses = () => {
