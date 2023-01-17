05048b18a8040d8c3f68b473eee8b139ddaa0fd9
Martynas Druteika <martynas.druteika@devbridge.com>
2023-01-17 20:00:03 +0200

    Logout implementation

--- client/src/components/PageLayout/PageLayout.jsx
+++ client/src/components/PageLayout/PageLayout.jsx
@@ -1,16 +1,33 @@
 import { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import { UserContext } from "../../contexts/UserContextWrapper";
+import { Button } from '../Button/Button';
+import { LOCAL_STORAGE_JWT_TOKEN_KEY } from '../../constants/constants';
+import styled from 'styled-components';
+
+const Header = styled.div`
+    padding: 10px 40px;
+`;
 
 export const PageLayout = () => {
-    const { user } = useContext(UserContext);
+    const { user, setUser } = useContext(UserContext);
+    const navigate = useNavigate();
 
     if (!user) {
         return <Navigate to="/login" />
     }
 
+    const handleLogout = () => {
+        localStorage.removeItem(LOCAL_STORAGE_JWT_TOKEN_KEY);
+        setUser(null);
+        navigate('/login');
+    }
+
     return (
         <div>
+            <Header>
+                <Button onClick={handleLogout}>Log out</Button>
+            </Header>
             <Outlet />
         </div>
     )
